59d8bf88f9f74cfaf58f5c444c39156b7507a827
sujayuniverse <sujayuniverse3@gmail.com.com>
2024-08-16 12:07:28 +0530

    16 aug

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.rtdashboard.com',
+  appId: 'com.bd.ucb.rtdashboard',
   appName: 'realtimedashboard',
   webDir: 'www'
 };
